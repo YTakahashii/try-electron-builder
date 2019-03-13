7cd2a534e49268e0d0cda453f8d896bf3d2850f0
髙橋 佑太 <yuta.takahashi@jbs.com>
2019-03-13 14:45:54 +0900

    feature: yarn deployでblobへデプロイするように
    # 🎉  : tada: Initial commit
    # 🔖  :bookmark: バージョンタグ
    # 🐛  :bug: バグ修正
    # ✏️  :pencil2: タイポ修正
    # 👍  :+1: 機能改善
    # ✨  :sparkles: 部分的な機能追加
    # 💄  :lipstick: UIやスタイルファイルのアップデート
    # 📝  :memo: ドキュメントやコメントの追加
    # ♻️  :recycle: リファクタリング
    # 🗑️  :wastebasket: 不要な機能・使われなくなった機能の削除
    # ✅  :rotating_light: テスト実装
    # 💚  :green_heart: テストやCIの修正・改善
    # 👕  :shirt: Lintエラーの修正やコードスタイルの修正
    # 🚀  :rocket: パフォーマンス改善
    # ➕  :heavy_plus_sign: 依存パッケージの追加
    # ➖  :heavy_minus_sign: 依存パッケージの削除
    # 🆙  :up: 依存パッケージのアップデート
    # 🔒  :lock: 新機能の公開範囲の制限
    # 👮  :cop: セキュリティ関連の改善
    # 🚧  :construction: WIP(Work In Progress)
    # 🙈 :see_no_evil: .gitignoreファイルの更新
    # 🏷️ :label: 型の追加やアップデート
    
    # ==== Format ====
    # :emoji: Subject
    #
    # Commit body...

---
+++ deploy.js
@@ -0,0 +1,149 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load()
+}
+
+const path = require('path')
+const storage = require('azure-storage')
+
+const blobService = storage.createBlobService()
+
+const listContainers = async () => {
+  return new Promise((resolve, reject) => {
+    blobService.listContainersSegmented(null, (err, data) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve({
+          message: `${data.entries.length} containers`,
+          containers: data.entries
+        })
+      }
+    })
+  })
+}
+
+const createContainer = async containerName => {
+  return new Promise((resolve, reject) => {
+    blobService.createContainerIfNotExists(
+      containerName,
+      { publicAccessLevel: 'blob' },
+      err => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve({ message: `Container '${containerName}' created` })
+        }
+      }
+    )
+  })
+}
+
+const uploadString = async (containerName, blobName, text) => {
+  return new Promise((resolve, reject) => {
+    blobService.createBlockBlobFromText(containerName, blobName, text, err => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve({ message: `Text "${text}" is written to blob storage` })
+      }
+    })
+  })
+}
+
+const uploadLocalFile = async (containerName, filePath) => {
+  return new Promise((resolve, reject) => {
+    const fullPath = path.resolve(filePath)
+    const blobName = path.basename(filePath)
+    blobService.createBlockBlobFromLocalFile(
+      containerName,
+      blobName,
+      fullPath,
+      err => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve({ message: `Local file "${filePath}" is uploaded` })
+        }
+      }
+    )
+  })
+}
+
+const listBlobs = async containerName => {
+  return new Promise((resolve, reject) => {
+    blobService.listBlobsSegmented(containerName, null, (err, data) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve({
+          message: `${data.entries.length} blobs in '${containerName}'`,
+          blobs: data.entries
+        })
+      }
+    })
+  })
+}
+
+const downloadBlob = async (containerName, blobName) => {
+  const dowloadFilePath = path.resolve(
+    './' + blobName.replace('.txt', '.downloaded.txt')
+  )
+  return new Promise((resolve, reject) => {
+    blobService.getBlobToText(containerName, blobName, (err, data) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve({ message: `Blob downloaded "${data}"`, text: data })
+      }
+    })
+  })
+}
+
+const deleteBlob = async (containerName, blobName) => {
+  return new Promise((resolve, reject) => {
+    blobService.deleteBlobIfExists(containerName, blobName, err => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve({ message: `Block blob '${blobName}' deleted` })
+      }
+    })
+  })
+}
+
+const deleteContainer = async containerName => {
+  return new Promise((resolve, reject) => {
+    blobService.deleteContainer(containerName, err => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve({ message: `Container '${containerName}' deleted` })
+      }
+    })
+  })
+}
+
+const execute = async () => {
+  const containerName = 'installer'
+  const localFilePath = './dist/try-electron-builder-0.1.0.dmg'
+  let response
+
+  console.log('Containers:')
+  response = await listContainers()
+  response.containers.forEach(container => console.log(` -  ${container.name}`))
+
+  const containerDoesNotExist =
+    response.containers.findIndex(
+      container => container.name === containerName
+    ) === -1
+
+  if (containerDoesNotExist) {
+    await createContainer(containerName)
+    console.log(`Container "${containerName}" is created`)
+  }
+
+  response = await uploadLocalFile(containerName, localFilePath)
+  console.log(response.message)
+}
+
+execute().then(() => console.log('Done')).catch(e => console.log(e))
